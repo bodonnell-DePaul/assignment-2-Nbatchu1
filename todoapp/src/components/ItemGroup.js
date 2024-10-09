0aa0408eb0bd1bdb2a1d22b8c552eb80a95601d5
Nbatchu1 <nbatchu1@depaul.edu>
2024-10-09 15:06:38 -0500

    Added Variant Changes

--- todoapp/src/components/ItemGroup.js
+++ todoapp/src/components/ItemGroup.js
@@ -5,21 +5,22 @@ import items from '../todoItems';
 function ItemGroup() {
     const [selectedIndex, setSelectedIndex] = useState(0);
 
-    const getActiveColor = (date) => {
-        if (date === 1)
-            return 'secondary';
-        else if (date === 2)
-            return 'success';
-        else if (date === 3)
-            return 'danger';
-        else
-            return 'primary';
-    }
+    const getActiveColor = (dueDate) => {
+    const currentDate = new Date();
+    const dueDateObj = new Date(dueDate);
+    const diffTime = Math.abs(dueDateObj - currentDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  
+    if (diffDays > 7) return 'primary';
+    if (diffDays <= 7 && diffDays > 4) return 'success';
+    if (diffDays <= 4 && diffDays > 2) return 'warning';
+    return 'danger';
+  };
     return (<ListGroup defaultActiveKey={0}>
         {
             items.map((item, i) => (
                 <ListGroup.Item
-                    variant={getActiveColor(i)}
+                    variant={getActiveColor(item.dueDate)}
                     eventKey={item.dueDate}
                     action={true}
                     active={i === selectedIndex}
